080a48389bd895ae106718fdef787ff8ae0f4756
Gideon <mwaniki59@gmail.com>
2017-12-09 11:42:41 -0600

    added mailing services with sendgrid amd images and content for other pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'home#index'
     get 'about', to: 'about#index'
     get 'services', to: 'services#index'
     get 'contact', to: 'contact#index'
