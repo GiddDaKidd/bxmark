849c604dc5d3c0144a404fd00b74883a7ad59c4f
Gideon <mwaniki59@gmail.com>
2017-12-09 19:46:19 -0600

    adding mailer changes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: "info@bxmark.net"
+
+ def welcome_email(user)
+   @user = user
+   mail(to: @user.email, subject: 'Thanks for signing up')
+ end
+end
 end
