7158e4695ab591d3543a5bf04dbd1f20a8e0a5fc
Gideon <mwaniki59@gmail.com>
2017-12-09 23:05:30 -0600

    fixing confirmable

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,2 @@
 class RegistrationsController < Devise::RegistrationsController
-  def after_sign_in_path_for(user)
-    stored_location_for(user) ||
-      if resource.is_a?(user) && resource.can_publish?
-        publisher_url
-      else
-        super
       end
