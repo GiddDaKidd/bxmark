c99e0306ec2e5cac36261097b8f62b0f604b1abc
Gideon <mwaniki59@gmail.com>
2017-12-09 23:41:37 -0600

    fixing issues

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,13 +12,5 @@ class UserMailer < ApplicationMailer
       subject: email.subject,
       body: email.body
     )
-
-    if email.has_attachments?
-      email.attachments.each do |attachment|
-        page.attachments.create({
-          file: attachment,
-          description: email.subject
-        })
-      end
-end
+  end
 end
