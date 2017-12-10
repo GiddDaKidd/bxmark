aa6cb5c35d62053a0908e8d71d51c4e09af31212
Gideon <mwaniki59@gmail.com>
2017-12-09 22:37:59 -0600

    fixing controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
-  def after_sign_in_path_for(:user)
-    stored_location_for(:user) ||
+  def after_sign_in_path_for(User)
+    stored_location_for(User) ||
       if resource.is_a?(User) && resource.can_publish?
         publisher_url
       else
